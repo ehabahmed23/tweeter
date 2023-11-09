97a8b3da499a6dee55c4a550b1b90e63028485a3
Ehab Ahmed <ehabahmed2456@gmail.com>
2023-11-09 01:35:59 -0500

    character counter added

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-  // --- our code goes here ---
+  $('textarea').on('input', function() {
+    let text = $(this).val();
+    let remaining = 140 - text.length;
+    console.log('Text length: ' + remaining);
+    let counter = $(this).parent().find(".counter");
+    counter.text(remaining);
+    if (remaining < 0) {
+      $(counter).css("color", "red");
+    } else {
+      $(counter).css("color", "inherit");
+    }
+  });
 });
